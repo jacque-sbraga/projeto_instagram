0963633853177157224cfa7d1ecbae543d538271
Jacqueline Santos Braga <jaquelinesantosbraga@gmail.com>
2021-01-06 23:16:45 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Página do intagram :happy:
+
+Olá! Este é meu o projeto para o bootcamp Decola Dev, nele utilizei HTML, CSS e JavaScript (para criar o efeito de transição das imagens).
---
+++ script.js
@@ -0,0 +1,25 @@
+function slideTransition(){
+    function firstImg(){
+        const firstImage = document.querySelector(".images img:first-child");
+        firstImage.classList.add("fader");
+    }
+    
+    function slide(){
+        const image = document.querySelector(".fader");
+    
+        if(image.nextElementSibling){
+            image.nextElementSibling.classList.add("fader");
+            image.classList.remove("fader");
+        }
+        else{
+            image.classList.remove("fader");
+            firstImg();
+        }
+    }
+    
+    firstImg();
+    setInterval(slide, 5000);
+}
+
+slideTransition();
+
