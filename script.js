c1c13f8b2f3bf2228cdead2cb9853f04deb1551d
Jacqueline Santos Braga <jaquelinesantosbraga@gmail.com>
2021-01-07 05:42:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-function slideTransition(){
-    function firstImg(){
-        const firstImage = document.querySelector(".images img:first-child");
-        firstImage.classList.add("fader");
-    }
-    
-    function slide(){
-        const image = document.querySelector(".fader");
-    
-        if(image.nextElementSibling){
-            image.nextElementSibling.classList.add("fader");
-            image.classList.remove("fader");
-        }
-        else{
-            image.classList.remove("fader");
-            firstImg();
-        }
-    }
-    
-    firstImg();
-    setInterval(slide, 5000);
+
+function firstImg(){
+    const firstImage = document.querySelector(".images img:first-child");
+    firstImage.classList.add("fader");
 }
 
-slideTransition();
+function slide(){
+    const image = document.querySelector(".fader");
+
+    if(image.nextElementSibling){
+        image.nextElementSibling.classList.add("fader");
+        image.classList.remove("fader");
+    }
+    else{
+        image.classList.remove("fader");
+        firstImg();
+    }
+}
+    
+firstImg();
+const timer = setInterval(slide, 5000);
+
+
 
